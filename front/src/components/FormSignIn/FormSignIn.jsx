cc0661d8a061efe483a2cb60bad73f80889a9406
EnzoIn <enzit93@hotmail.fr>
2024-05-22 08:36:11 +0200

    Mise en place du store Redux , création de userSlice et du formulaire de connexion

--- front/src/components/FormSignIn/FormSignIn.jsx
+++ front/src/components/FormSignIn/FormSignIn.jsx
@@ -1,23 +1,77 @@
-import React from 'react';
-import './formSignIn.css'
+import axios from "axios";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { login } from "../../redux/reducer/userSlice";
+import "./formSignIn.css";
 
 const FormSignIn = () => {
+  const [email, setEmail] = useState("tony@stark.com");
+  const [password, setPassword] = useState("password123");
+  const [rememberMe, setRememberMe] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:3001/api/v1/user/login",
+        {
+          email,
+          password,
+        }
+      );
+
+      console.log(response.data.body.token);
+      if (response.status === 200) {
+        const token = response.data.body.token;
+        if (rememberMe) {
+          localStorage.setItem("token", token);
+        }
+        navigate("/user");
+        dispatch(login({ token }));
+      }
+    } catch (error) {
+      console.error("Erreur de connexion:", error);
+    }
+  };
+
   return (
-    <form>
-        <div className="input-wrapper">
-          <label htmlFor="username">Username</label>
-          <input type="text" id="username" />
-        </div>
-        <div className="input-wrapper">
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" />
-        </div>
-        <div className="input-remember">
-          <input type="checkbox" id="remember-me" />
-          <label htmlFor="remember-me">Remember me</label>
-        </div>
-        <button type="submit" className="sign-in-button">Sign In</button>
-      </form>
+    <form onSubmit={handleSubmit}>
+      <div className="input-wrapper">
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+      <div className="input-wrapper">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+      <div className="input-remember">
+        <input
+          type="checkbox"
+          id="remember-me"
+          checked={rememberMe}
+          onChange={(e) => setRememberMe(e.target.checked)}
+        />
+        <label htmlFor="remember-me">Remember me</label>
+      </div>
+      <button type="submit" className="sign-in-button">
+        Sign In
+      </button>
+    </form>
   );
 };
 
--- front/src/pages/User/User.jsx
+++ front/src/pages/User/User.jsx
@@ -4,12 +4,12 @@ import './user.css';
 
 const User = () => {
   return (
-    <main class="main bg-dark">
-      <div class="header">
+    <main className="main bg-dark">
+      <div className="header">
         <h1>Welcome back<br />Tony Jarvis!</h1>
-        <button class="edit-button">Edit Name</button>
+        <button className="edit-button">Edit Name</button>
       </div>
-      <h2 class="sr-only">Accounts</h2>
+      <h2 className="sr-only">Accounts</h2>
       <Account />
       <Account />
       <Account />
