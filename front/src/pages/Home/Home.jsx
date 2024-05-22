130721194642ba9edf777975080b0210bb563555
EnzoIn <enzit93@hotmail.fr>
2024-05-22 09:29:47 +0200

    Conversion des images en WebP, fix (Features, Home)

--- front/src/pages/Home/Home.jsx
+++ front/src/pages/Home/Home.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import Banner from '../../components/Banner/Banner';
-import Features from '../../components/Features/Features';
+import React from "react";
+import Banner from "../../components/Banner/Banner";
+import featuresData from "../../components/Features/features.json";
+import Features from "../../components/Features/Features";
 
 const Home = () => {
   return (
     <main>
-    <Banner />
-    <Features />
+      <Banner />
+      <section className="features">
+        <h2 className="sr-only">Features</h2>
+        {featuresData.map((element) => <Features element={element} key={element.title} />)}
+      </section>
     </main>
   );
 };
